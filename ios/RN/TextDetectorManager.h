6ea7ba301dd7f6dd516a9738c1771b01497cc267
Andy <fischer@integreat-app.de>
2022-04-01 18:35:03 +0200

    fix tdm import

--- ios/RN/TextDetectorManager.h
+++ ios/RN/TextDetectorManager.h
@@ -1,12 +1,13 @@
 #if __has_include(<MLKitTextRecognition/MLKitTextRecognition.h>)
-  @import MLKitTextRecognition;
+@import MLKitTextRecognition;
+@import MLKitTextRecognitionCommon;
 #endif
-  @interface TextDetectorManager : NSObject
-  typedef void(^postRecognitionBlock)(NSArray *textBlocks);
+@interface TextDetectorManager : NSObject
+typedef void(^postRecognitionBlock)(NSArray *textBlocks);
 
-  - (instancetype)init;
+- (instancetype)init;
 
-  -(BOOL)isRealDetector;
-  -(void)findTextBlocksInFrame:(UIImage *)image scaleX:(float)scaleX scaleY:(float)scaleY completed:(postRecognitionBlock)completed;
+-(BOOL)isRealDetector;
+-(void)findTextBlocksInFrame:(UIImage *)image scaleX:(float)scaleX scaleY:(float)scaleY completed:(postRecognitionBlock)completed;
 
-  @end
+@end
